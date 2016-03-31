e5c4b3158af7b82e40402a64e34f5f5efd6c352f
practicing01 <practicing01@gmail.com>
2016-03-31 19:41:40 -0400

    skills progress

--- server/dotsnetcrits/gamemodes/skills/bar3/slot0/slot0.cs
+++ server/dotsnetcrits/gamemodes/skills/bar3/slot0/slot0.cs
@@ -13,6 +13,11 @@ function SilenceSkillsGM::Action(%this, %client, %guiSlot)
 
   %obj = firstWord(%rayResult);
 
+  if (!isObject(%obj))
+  {
+    return;
+  }
+
   if (%obj.getClassName() $= "Player" || %obj.getClassName() $= "AIPlayer")
   {
     if (!%obj.isField("silenceSet_"))
@@ -21,21 +26,57 @@ function SilenceSkillsGM::Action(%this, %client, %guiSlot)
       %this.silenceSets_.add(%obj.silenceSet_);
     }
 
+    %targetEmitterNode = new ParticleEmitterNode()
+    {
+      datablock = SilenceEmitterNodeData;
+      emitter = SilenceEmitter;
+      active = true;
+      velocity = 0.0;
+    };
+
     %silence = new ScriptObject()
     {
       class = "SilenceInstanceSkillsGM";
+      emitterNode_ = %targetEmitterNode;
     };
+
+    %obj.mountObject(%targetEmitterNode, GetMountIndexDNC(%obj, 0));
 
     %obj.silenceSet_.add(%silence);
 
+    if (isObject(%this.emitterNode_))
+    {
+      %this.emitterNode_.delete();
+    }
+
+    %this.emitterNode_ = new ParticleEmitterNode()
+    {
+      datablock = SilenceEmitterNodeData;
+      emitter = SilenceEmitter;
+      active = true;
+      velocity = 0.0;
+    };
+
+    %player.mountObject(%this.emitterNode_, GetMountIndexDNC(%player, 0));
+
+    %this.schedule(%this.emitterDuration_ * 1000, "RemoveEmitter");
+
     %silence.schedule(%this.duration_ * 1000, "delete");
+
+    %this.cooling_ = true;
+
+    %this.schedule(%this.coolDownTime_ * 1000, "CoolDown");
+
   }
 
 }
 
 function SilenceInstanceSkillsGM::onRemove(%this)
 {
-  echo("SilenceInstanceSkillsGM removed");
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
 }
 
 function SilenceSkillsGM::onAdd(%this)
@@ -48,6 +89,21 @@ function SilenceSkillsGM::onRemove(%this)
   %this.silenceSets_.callOnChildren("deleteAllObjects");
   %this.silenceSets_.deleteAllObjects();
   %this.silenceSets_.delete();
+
+  cancel(%this.cdSchedule_);
+}
+
+function SilenceSkillsGM::RemoveEmitter(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function SilenceSkillsGM::CoolDown(%this)
+{
+  %this.cooling_ = false;
 }
 
 $skill = "";
@@ -58,9 +114,13 @@ if (isObject(SkillsGMServerSO))
   {
     class = "SilenceSkillsGM";
     guiSlot_ = "";
-    coolDownTime_ = 1.0;
+    coolDownTime_ = 5.0;
     cooling_ = false;
     duration_ = 5.0;
     silenceSets_ = "";
+    cdSchedule_ = "";
+    emitterNode_ = "";
+    emitterDuration_ = 1.0;
+    emitterSchedule_ = "";
   };
 }
