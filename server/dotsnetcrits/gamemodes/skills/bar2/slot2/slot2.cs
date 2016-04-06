01b6f7aabbfffc3180448f8260275a3678223be5
practicing01 <practicing01@gmail.com>
2016-04-05 20:21:52 -0400

    hot skill

--- server/dotsnetcrits/gamemodes/skills/bar2/slot2/slot2.cs
+++ server/dotsnetcrits/gamemodes/skills/bar2/slot2/slot2.cs
@@ -1,7 +1,167 @@
 function HOTSkillsGM::Action(%this, %client, %guiSlot)
 {
+  if (%this.cooling_)
+  {
+    return;
+  }
+
   %this.guiSlot_ = %guiSlot;
-  echo(%this.guiSlot_.getName());
+
+  %player = %client.getControlObject();
+
+  if (%player.isField("hotSet_"))
+  {
+    if (%player.hotSet_.getCount() > 0)
+    {
+      return;
+    }
+  }
+
+  %rayResult = %player.doRaycast(1000.0, $TypeMasks::ShapeBaseObjectType);
+
+  %obj = firstWord(%rayResult);
+
+  if (!isObject(%obj))
+  {
+    return;
+  }
+
+  if (%obj.getClassName() $= "Player" || %obj.getClassName() $= "AIPlayer")
+  {
+    if (!%obj.isField("hotSet_"))
+    {
+      %obj.hotSet_ = new SimSet();
+      %this.hotSets_.add(%obj.hotSet_);
+    }
+
+    %targetEmitterNode = new ParticleEmitterNode()
+    {
+      datablock = HOTEmitterNodeData;
+      emitter = HOTEmitter;
+      active = true;
+      velocity = 0.0;
+    };
+
+    %hot = new ScriptObject()
+    {
+      class = "HOTInstanceSkillsGM";
+      emitterNode_ = %targetEmitterNode;
+      pulseInterval_ = 1.0;
+      pulseIntervalCount_ = 0;
+      pulseDuration_ = %this.duration_;
+      target_ = %obj;
+      power_ = 10.0;
+    };
+
+    %obj.mountObject(%targetEmitterNode, GetMountIndexDNC(%obj, 0));
+
+    %obj.hotSet_.add(%hot);
+
+    if (isObject(%this.emitterNode_))
+    {
+      %this.emitterNode_.delete();
+    }
+
+    %this.emitterNode_ = new ParticleEmitterNode()
+    {
+      datablock = HOTEmitterNodeData;
+      emitter = HOTEmitter;
+      active = true;
+      velocity = 0.0;
+    };
+
+    %player.mountObject(%this.emitterNode_, GetMountIndexDNC(%player, 0));
+
+    %this.schedule(%this.emitterDuration_ * 1000, "RemoveEmitter");
+
+    %hot.schedule(0, "Pulse");
+    %hot.schedule(%this.duration_ * 1000, "delete");
+
+    %this.cooling_ = true;
+    //%this.guiSlot_.setText(%this.coolDownTime_);
+    %this.coolDownElapsedTime_ = 0.0;
+    %this.schedule(1000, "CoolDown");
+
+    %player.setActionThread("Celebrate_01", false);
+  }
+
+}
+
+function HOTInstanceSkillsGM::Pulse(%this)
+{
+  if (%this.pulseIntervalCount_ >= %this.pulseDuration_)
+  {
+    return;
+  }
+
+  %this.pulseIntervalCount_++;
+
+  if (isObject(%this.target_))
+  {
+    /*%objArmor = 0;
+
+    if (%this.target_.isField("shieldSet_"))
+    {
+      for (%x = 0; %x < %this.target_.shieldSet_.getCount(); %x++)
+      {
+        %objArmor += %this.target_.shieldSet_.getObject(%x).power_;
+      }
+    }
+
+    %this.target_.damage(%this, %this.target_.position, getMax(0, %this.power_ - %objArmor), "hot");*/
+
+    %this.target_.applyRepair(%this.power_);
+  }
+
+  %this.schedule(%this.pulseInterval_ * 1000, "Pulse");
+}
+
+function HOTInstanceSkillsGM::onRemove(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function HOTSkillsGM::onAdd(%this)
+{
+  %this.hotSets_ = new SimSet();
+}
+
+function HOTSkillsGM::onRemove(%this)
+{
+  if (isObject(%this.hotSets_))
+  {
+    %this.hotSets_.callOnChildren("deleteAllObjects");
+    %this.hotSets_.deleteAllObjects();
+    %this.hotSets_.delete();
+  }
+
+  cancel(%this.cdSchedule_);
+}
+
+function HOTSkillsGM::RemoveEmitter(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function HOTSkillsGM::CoolDown(%this)
+{
+  %this.coolDownElapsedTime_ += 1.0;
+
+  //%this.guiSlot_.setText(%this.coolDownTime_ - %this.coolDownElapsedTime_);
+
+  if (%this.coolDownElapsedTime_ >= %this.coolDownTime_)
+  {
+    %this.cooling_ = false;
+    return;
+  }
+
+  %this.schedule(1000, "CoolDown");
 }
 
 $skill = "";
@@ -12,7 +172,14 @@ if (isObject(SkillsGMServerSO))
   {
     class = "HOTSkillsGM";
     guiSlot_ = "";
-    coolDownTime_ = 1.0;
+    coolDownTime_ = 5.0;
+    coolDownElapsedTime_ = 0.0;
     cooling_ = false;
+    duration_ = 5.0;
+    hotSets_ = "";
+    cdSchedule_ = "";
+    emitterNode_ = "";
+    emitterDuration_ = 1.0;
+    emitterSchedule_ = "";
   };
 }
