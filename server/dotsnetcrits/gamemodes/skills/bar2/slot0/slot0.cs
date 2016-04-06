a40489beed17b3fb9461ff5fec3f722e29a275a0
practicing01 <practicing01@gmail.com>
2016-04-05 20:14:31 -0400

    heal and poison skills

--- server/dotsnetcrits/gamemodes/skills/bar2/slot0/slot0.cs
+++ server/dotsnetcrits/gamemodes/skills/bar2/slot0/slot0.cs
@@ -1,7 +1,146 @@
 function HealSkillsGM::Action(%this, %client, %guiSlot)
 {
+  if (%this.cooling_)
+  {
+    return;
+  }
+
+  %player = %client.getControlObject();
+
+  if (%player.isField("blindSet_"))
+  {
+    if (%player.blindSet_.getCount() > 0)
+    {
+      return;
+    }
+  }
+
+  %pos = %player.getPosition();
+
   %this.guiSlot_ = %guiSlot;
-  echo(%this.guiSlot_.getName());
+
+  initContainerRadiusSearch(%pos, %this.sphereCastRadius_, $TypeMasks::ShapeBaseObjectType);
+
+  while ( (%obj = containerSearchNext()) != 0 )
+  {
+    if (%obj.getClassName() $= "Player" || %obj.getClassName() $= "AIPlayer")
+    {
+      /*if (%obj == %player)
+      {
+        continue;
+      }*/
+
+      if (!%obj.isField("healSet_"))
+      {
+        %obj.healSet_ = new SimSet();
+        %this.healSets_.add(%obj.healSet_);
+      }
+
+      %targetEmitterNode = new ParticleEmitterNode()
+      {
+        datablock = HealEmitterNodeData;
+        emitter = HealEmitter;
+        active = true;
+        velocity = 0.0;
+      };
+
+      %heal = new ScriptObject()
+      {
+        class = "HealInstanceSkillsGM";
+        emitterNode_ = %targetEmitterNode;
+      };
+
+      %obj.mountObject(%targetEmitterNode, GetMountIndexDNC(%obj, 0));
+
+      %obj.healSet_.add(%heal);
+
+      %heal.schedule(%this.duration_ * 1000, "delete");
+
+      /*%objArmor = 0;
+
+      if (%obj.isField("shieldSet_"))
+      {
+        for (%x = 0; %x < %obj.shieldSet_.getCount(); %x++)
+        {
+          %objArmor += %obj.shieldSet_.getObject(%x).power_;
+        }
+      }*/
+
+      %obj.applyRepair(%this.power_);
+    }
+  }
+
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+
+  %this.emitterNode_ = new ParticleEmitterNode()
+  {
+    datablock = HealEmitterNodeData;
+    emitter = HealEmitter;
+    active = true;
+    velocity = 0.0;
+  };
+
+  %player.mountObject(%this.emitterNode_, GetMountIndexDNC(%player, 0));
+
+  %this.schedule(%this.emitterDuration_ * 1000, "RemoveEmitter");
+
+  %this.cooling_ = true;
+  //%this.guiSlot_.setText(%this.coolDownTime_);
+  %this.coolDownElapsedTime_ = 0.0;
+  %this.schedule(1000, "CoolDown");
+
+  %player.setActionThread("Celebrate_01", false);
+}
+
+function HealInstanceSkillsGM::onRemove(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function HealSkillsGM::onAdd(%this)
+{
+  %this.healSets_ = new SimSet();
+}
+
+function HealSkillsGM::onRemove(%this)
+{
+  if (isObject(%this.healSets_))
+  {
+    %this.healSets_.callOnChildren("deleteAllObjects");
+    %this.healSets_.deleteAllObjects();
+    %this.healSets_.delete();
+  }
+
+  cancel(%this.cdSchedule_);
+}
+
+function HealSkillsGM::RemoveEmitter(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function HealSkillsGM::CoolDown(%this)
+{
+  %this.coolDownElapsedTime_ += 1.0;
+
+  //%this.guiSlot_.setText(%this.coolDownTime_ - %this.coolDownElapsedTime_);
+
+  if (%this.coolDownElapsedTime_ >= %this.coolDownTime_)
+  {
+    %this.cooling_ = false;
+    return;
+  }
+
+  %this.schedule(1000, "CoolDown");
 }
 
 $skill = "";
@@ -13,6 +152,15 @@ if (isObject(SkillsGMServerSO))
     class = "HealSkillsGM";
     guiSlot_ = "";
     coolDownTime_ = 1.0;
+    coolDownElapsedTime_ = 0.0;
     cooling_ = false;
+    duration_ = 1.0;
+    healSets_ = "";
+    cdSchedule_ = "";
+    emitterNode_ = "";
+    emitterDuration_ = 1.0;
+    emitterSchedule_ = "";
+    sphereCastRadius_ = 2.0;
+    power_ = 10.0;
   };
 }
--- server/dotsnetcrits/gamemodes/skills/bar3/slot2/slot2.cs
+++ server/dotsnetcrits/gamemodes/skills/bar3/slot2/slot2.cs
@@ -1,7 +1,165 @@
 function PoisonSkillsGM::Action(%this, %client, %guiSlot)
 {
+  if (%this.cooling_)
+  {
+    return;
+  }
+
   %this.guiSlot_ = %guiSlot;
-  echo(%this.guiSlot_.getName());
+
+  %player = %client.getControlObject();
+
+  if (%player.isField("poisonSet_"))
+  {
+    if (%player.poisonSet_.getCount() > 0)
+    {
+      return;
+    }
+  }
+
+  %rayResult = %player.doRaycast(1000.0, $TypeMasks::ShapeBaseObjectType);
+
+  %obj = firstWord(%rayResult);
+
+  if (!isObject(%obj))
+  {
+    return;
+  }
+
+  if (%obj.getClassName() $= "Player" || %obj.getClassName() $= "AIPlayer")
+  {
+    if (!%obj.isField("poisonSet_"))
+    {
+      %obj.poisonSet_ = new SimSet();
+      %this.poisonSets_.add(%obj.poisonSet_);
+    }
+
+    %targetEmitterNode = new ParticleEmitterNode()
+    {
+      datablock = PoisonEmitterNodeData;
+      emitter = PoisonEmitter;
+      active = true;
+      velocity = 0.0;
+    };
+
+    %poison = new ScriptObject()
+    {
+      class = "PoisonInstanceSkillsGM";
+      emitterNode_ = %targetEmitterNode;
+      pulseInterval_ = 1.0;
+      pulseIntervalCount_ = 0;
+      pulseDuration_ = %this.duration_;
+      target_ = %obj;
+      power_ = 10.0;
+    };
+
+    %obj.mountObject(%targetEmitterNode, GetMountIndexDNC(%obj, 0));
+
+    %obj.poisonSet_.add(%poison);
+
+    if (isObject(%this.emitterNode_))
+    {
+      %this.emitterNode_.delete();
+    }
+
+    %this.emitterNode_ = new ParticleEmitterNode()
+    {
+      datablock = PoisonEmitterNodeData;
+      emitter = PoisonEmitter;
+      active = true;
+      velocity = 0.0;
+    };
+
+    %player.mountObject(%this.emitterNode_, GetMountIndexDNC(%player, 0));
+
+    %this.schedule(%this.emitterDuration_ * 1000, "RemoveEmitter");
+
+    %poison.schedule(0, "Pulse");
+    %poison.schedule(%this.duration_ * 1000, "delete");
+
+    %this.cooling_ = true;
+    //%this.guiSlot_.setText(%this.coolDownTime_);
+    %this.coolDownElapsedTime_ = 0.0;
+    %this.schedule(1000, "CoolDown");
+
+    %player.setActionThread("Celebrate_01", false);
+  }
+
+}
+
+function PoisonInstanceSkillsGM::Pulse(%this)
+{
+  if (%this.pulseIntervalCount_ >= %this.pulseDuration_)
+  {
+    return;
+  }
+
+  %this.pulseIntervalCount_++;
+
+  if (isObject(%this.target_))
+  {
+    %objArmor = 0;
+
+    if (%this.target_.isField("shieldSet_"))
+    {
+      for (%x = 0; %x < %this.target_.shieldSet_.getCount(); %x++)
+      {
+        %objArmor += %this.target_.shieldSet_.getObject(%x).power_;
+      }
+    }
+
+    %this.target_.damage(%this, %this.target_.position, getMax(0, %this.power_ - %objArmor), "poison");
+  }
+
+  %this.schedule(%this.pulseInterval_ * 1000, "Pulse");
+}
+
+function PoisonInstanceSkillsGM::onRemove(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function PoisonSkillsGM::onAdd(%this)
+{
+  %this.poisonSets_ = new SimSet();
+}
+
+function PoisonSkillsGM::onRemove(%this)
+{
+  if (isObject(%this.poisonSets_))
+  {
+    %this.poisonSets_.callOnChildren("deleteAllObjects");
+    %this.poisonSets_.deleteAllObjects();
+    %this.poisonSets_.delete();
+  }
+
+  cancel(%this.cdSchedule_);
+}
+
+function PoisonSkillsGM::RemoveEmitter(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function PoisonSkillsGM::CoolDown(%this)
+{
+  %this.coolDownElapsedTime_ += 1.0;
+
+  //%this.guiSlot_.setText(%this.coolDownTime_ - %this.coolDownElapsedTime_);
+
+  if (%this.coolDownElapsedTime_ >= %this.coolDownTime_)
+  {
+    %this.cooling_ = false;
+    return;
+  }
+
+  %this.schedule(1000, "CoolDown");
 }
 
 $skill = "";
@@ -12,7 +170,14 @@ if (isObject(SkillsGMServerSO))
   {
     class = "PoisonSkillsGM";
     guiSlot_ = "";
-    coolDownTime_ = 1.0;
+    coolDownTime_ = 5.0;
+    coolDownElapsedTime_ = 0.0;
     cooling_ = false;
+    duration_ = 5.0;
+    poisonSets_ = "";
+    cdSchedule_ = "";
+    emitterNode_ = "";
+    emitterDuration_ = 1.0;
+    emitterSchedule_ = "";
   };
 }
