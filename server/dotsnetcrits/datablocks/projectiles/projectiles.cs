8efe76c09be03b66b87a2d189206f28e5e473e0d
practicing01 <practicing01@gmail.com>
2016-04-09 15:39:02 -0400

    projectile skill

---
+++ server/dotsnetcrits/datablocks/projectiles/projectiles.cs
@@ -0,0 +1,33 @@
+datablock ProjectileData(RangedSkillsGMProjectile)
+{
+   projectileShapeName = "art/shapes/weapons/shared/rocket.dts";
+   directDamage = 10;
+   radiusDamage = 10;
+   damageRadius = 5;
+   areaImpulse = 2000;
+
+   explosion = GrenadeLauncherExplosion;
+   waterExplosion = GrenadeLauncherWaterExplosion;
+
+   decal = ScorchRXDecal;
+   splash = GrenadeSplash;
+
+   particleEmitter = GrenadeProjSmokeTrailEmitter;
+   particleWaterEmitter = GrenadeTrailWaterEmitter;
+
+   muzzleVelocity = 1;
+   velInheritFactor = 0.3;
+
+   armingDelay = 1;
+   lifetime = 60000;
+   fadeDelay = 4500;
+
+   bounceElasticity = 1.0;
+   bounceFriction = 0.0;
+   isBallistic = true;
+   gravityMod = 0;
+
+   lightDesc = GrenadeLauncherLightDesc;
+
+   damageType = "RangedSkillsGM";
+};
--- server/dotsnetcrits/gamemodes/skills/bar0/slot3/slot3.cs
+++ server/dotsnetcrits/gamemodes/skills/bar0/slot3/slot3.cs
@@ -1,7 +1,146 @@
 function RangedSkillsGM::Action(%this, %client, %guiSlot)
 {
+  if (%this.cooling_)
+  {
+    return;
+  }
+
   %this.guiSlot_ = %guiSlot;
-  echo(%this.guiSlot_.getName());
+
+  %player = %client.getControlObject();
+
+  if (%player.isField("blindSet_"))
+  {
+    if (%player.blindSet_.getCount() > 0)
+    {
+      return;
+    }
+  }
+
+  //%rayResult = %player.doRaycast(1000.0, $TypeMasks::ShapeBaseObjectType);
+
+  //%obj = firstWord(%rayResult);
+
+  /*if (!isObject(%obj))
+  {
+    return;
+  }*/
+
+  %obj = %player;
+
+  if (%obj.getClassName() $= "Player" || %obj.getClassName() $= "AIPlayer")
+  {
+    if (!%obj.isField("rangedSet_"))
+    {
+      %obj.rangedSet_ = new SimSet();
+      %this.rangedSets_.add(%obj.rangedSet_);
+    }
+
+    %targetEmitterNode = new ParticleEmitterNode()
+    {
+      datablock = RangedEmitterNodeData;
+      emitter = RangedEmitter;
+      active = true;
+      velocity = 0.0;
+    };
+
+    %ranged = new ScriptObject()
+    {
+      class = "RangedInstanceSkillsGM";
+      emitterNode_ = %targetEmitterNode;
+    };
+
+    %obj.mountObject(%targetEmitterNode, GetMountIndexDNC(%obj, 0));
+
+    %obj.rangedSet_.add(%ranged);
+
+    if (isObject(%this.emitterNode_))
+    {
+      %this.emitterNode_.delete();
+    }
+
+    %this.emitterNode_ = new ParticleEmitterNode()
+    {
+      datablock = RangedEmitterNodeData;
+      emitter = RangedEmitter;
+      active = true;
+      velocity = 0.0;
+    };
+
+    %player.mountObject(%this.emitterNode_, GetMountIndexDNC(%player, 0));
+
+    %this.schedule(%this.emitterDuration_ * 1000, "RemoveEmitter");
+
+    %ranged.schedule(%this.duration_ * 1000, "delete");
+
+    %this.cooling_ = true;
+    //%this.guiSlot_.setText(%this.coolDownTime_);
+    %this.coolDownElapsedTime_ = 0.0;
+    %this.schedule(1000, "CoolDown");
+
+    %player.setActionThread("Celebrate_01", false);
+
+    %projectileVelocity = VectorScale(%player.getEyeVector(), %this.velocityMagnitude_);
+
+    %projectile = new Projectile()
+    {
+      datablock = RangedSkillsGMProjectile;
+      initialPosition = %player.getEyePoint();
+      initialVelocity = %projectileVelocity;
+      sourceObject = %player;
+      sourceSlot = 0;
+      client = %player.client;
+    };
+  }
+
+}
+
+function RangedInstanceSkillsGM::onRemove(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function RangedSkillsGM::onAdd(%this)
+{
+  %this.rangedSets_ = new SimSet();
+}
+
+function RangedSkillsGM::onRemove(%this)
+{
+  if (isObject(%this.rangedSets_))
+  {
+    %this.rangedSets_.callOnChildren("deleteAllObjects");
+    %this.rangedSets_.deleteAllObjects();
+    %this.rangedSets_.delete();
+  }
+
+  cancel(%this.cdSchedule_);
+}
+
+function RangedSkillsGM::RemoveEmitter(%this)
+{
+  if (isObject(%this.emitterNode_))
+  {
+    %this.emitterNode_.delete();
+  }
+}
+
+function RangedSkillsGM::CoolDown(%this)
+{
+  %this.coolDownElapsedTime_ += 1.0;
+
+  //%this.guiSlot_.setText(%this.coolDownTime_ - %this.coolDownElapsedTime_);
+
+  if (%this.coolDownElapsedTime_ >= %this.coolDownTime_)
+  {
+    %this.cooling_ = false;
+    return;
+  }
+
+  %this.schedule(1000, "CoolDown");
 }
 
 $skill = "";
@@ -12,7 +151,15 @@ if (isObject(SkillsGMServerSO))
   {
     class = "RangedSkillsGM";
     guiSlot_ = "";
-    coolDownTime_ = 1.0;
+    coolDownTime_ = 5.0;
+    coolDownElapsedTime_ = 0.0;
     cooling_ = false;
+    duration_ = 1.0;
+    rangedSets_ = "";
+    cdSchedule_ = "";
+    emitterNode_ = "";
+    emitterDuration_ = 1.0;
+    emitterSchedule_ = "";
+    velocityMagnitude_ = 2.0;
   };
 }
